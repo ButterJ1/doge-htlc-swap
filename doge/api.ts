20a539e01ea69644e48d6b27b5793ff2de05f3da
ButterJ1 <coconutbutter1333@gmail.com>
2026-03-15 15:28:53 +0800

    Update

--- doge/api.ts
+++ doge/api.ts
@@ -1,79 +1,93 @@
 import axios from 'axios';
-export const TESTNET_API = 'https://doge-electrs-testnet-demo.qed.me';
-export const MAINNET_API = 'https://dogechain.info/api/v1';
+
+const BASE = 'https://doge-electrs-testnet-demo.qed.me';
 
 export interface UTXO {
   txid:   string;
   vout:   number;
   value:  number;
-  status: {
-    confirmed:    boolean;
-    block_height: number;
-    block_hash:   string;
-    block_time:   number;
-  };
+  status: { confirmed: boolean; block_height?: number };
 }
 
-export interface AddressInfo {
-  address:              string;
-  chain_stats: {
-    funded_txo_sum:  number;
-    spent_txo_sum:   number;
-    tx_count:        number;
-  };
-  mempool_stats: {
-    funded_txo_sum:  number;
-    spent_txo_sum:   number;
-    tx_count:        number;
-  };
+export async function getUTXOs(address: string): Promise<UTXO[]> {
+  const res = await axios.get<UTXO[]>(`${BASE}/address/${address}/utxo`);
+  return res.data;
+}
+
+export async function getTxHex(txid: string): Promise<string> {
+  const res = await axios.get<string>(`${BASE}/tx/${txid}/hex`);
+  return res.data;
+}
+
+export async function broadcast(txHex: string): Promise<string> {
+  try {
+    const res = await axios.post<string>(`${BASE}/tx`, txHex, {
+      headers: { 'Content-Type': 'text/plain' },
+    });
+    return res.data;
+  } catch (err: any) {
+    const detail = err.response?.data ?? err.message;
+    throw new Error(`broadcast failed: ${detail}`);
+  }
+}
+
+export async function getBalance(address: string): Promise<number> {
+  const utxos = await getUTXOs(address);
+  return utxos.reduce((sum, u) => sum + u.value, 0);
+}
+
+export function explorerUrl(txid: string): string {
+  return `https://doge-testnet-explorer.qed.me/tx/${txid}`;
+}
+
+export interface OutspendStatus {
+  spent:  boolean;
+  txid?:  string;
+  vin?:   number;
+  status?: { confirmed: boolean; block_height?: number };
+}
+
+export async function getOutspend(
+  fundingTxid: string,
+  fundingVout: number,
+): Promise<OutspendStatus> {
+  const res = await axios.get<OutspendStatus>(
+    `${BASE}/tx/${fundingTxid}/outspend/${fundingVout}`,
+  );
+  return res.data;
+}
+
+export interface TxVin {
+  txid:         string;
+  vout:         number;
+  scriptsig:    string;
+  scriptsig_asm?: string;
+  sequence:     number;
+}
+
+export interface TxDetails {
+  txid:     string;
+  version:  number;
+  locktime: number;
+  vin:      TxVin[];
+  vout:     Array<{ value: number; scriptpubkey: string }>;
+  status:   { confirmed: boolean; block_height?: number };
+}
+
+export async function getTxDetails(txid: string): Promise<TxDetails> {
+  const res = await axios.get<TxDetails>(`${BASE}/tx/${txid}`);
+  return res.data;
 }
 
 export class ElectrsAPI {
-  private base: string;
+  constructor(_baseUrl: string = BASE) {}
 
-  constructor(baseUrl: string = TESTNET_API) {
-    this.base = baseUrl.replace(/\/$/, '');
-  }
-
-  async getUTXOs(address: string): Promise<UTXO[]> {
-    const url = `${this.base}/address/${address}/utxo`;
-    const res = await axios.get<UTXO[]>(url);
-    return res.data;
-  }
-
-  async getAddressInfo(address: string): Promise<AddressInfo> {
-    const url = `${this.base}/address/${address}`;
-    const res = await axios.get<AddressInfo>(url);
-    return res.data;
-  }
-
-  async getTxHex(txid: string): Promise<string> {
-    const url = `${this.base}/tx/${txid}/hex`;
-    const res = await axios.get<string>(url);
-    return res.data;
-  }
-
-  async broadcast(txHex: string): Promise<string> {
-    const url = `${this.base}/tx`;
-    try {
-      const res = await axios.post<string>(url, txHex, {
-        headers: { 'Content-Type': 'text/plain' },
-      });
-      return res.data;
-    } catch (err: unknown) {
-      if (axios.isAxiosError(err) && err.response) {
-        throw new Error(
-          `Broadcast failed [${err.response.status}]: ${JSON.stringify(err.response.data)}`,
-        );
-      }
-      throw err;
-    }
-  }
-
-  async getBalance(address: string): Promise<number> {
-    const utxos = await this.getUTXOs(address);
-    return utxos.reduce((sum, u) => sum + u.value, 0);
-  }
+  getUTXOs(address: string)               { return getUTXOs(address); }
+  getTxHex(txid: string)                  { return getTxHex(txid); }
+  broadcast(txHex: string)                { return broadcast(txHex); }
+  getBalance(address: string)             { return getBalance(address); }
+  getOutspend(txid: string, vout: number) { return getOutspend(txid, vout); }
+  getTxDetails(txid: string)              { return getTxDetails(txid); }
 }
 
-export const testnetAPI = new ElectrsAPI(TESTNET_API);
+export const testnetAPI = new ElectrsAPI();
